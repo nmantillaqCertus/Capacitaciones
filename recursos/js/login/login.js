64f559f7e9000173a971021f25be012a22c5af00
César Mantilla <90099252+nmantillaqCertus@users.noreply.github.com>
2023-09-27 10:24:20 -0500

    commit bandeja

--- recursos/js/login/login.js
+++ recursos/js/login/login.js
@@ -1,43 +1,150 @@
 
 
+$(".finit").on('click', function () {
+    //debugger
+    if (validarFormulario()) {
+        let aux_Usuario = $("#ID_Usuario").val().trim();
+        let aux_Contrasenia = $("#ID_Contrasenia").val();
+        let respuestaUsuario = validarUsaurio(aux_Usuario, aux_Contrasenia);
+        if (respuestaUsuario.existe && respuestaUsuario.contrasenia) {
 
-$(".finit").on('click', function(){
+            console.log(respuestaUsuario);
 
-    debugger
-
-     if(validarFormulario()){
-        let respuestaUsuario = validarUsaurio();
-
-        if(respuestaUsuario.existe && respuestaUsuario.contrasenia){
-            iniciarAPP();
-        }else{
-            //lanzar mensajes
-            //Usuario contraseña incorrecta
+            $("#ID_MsjError").hide();
+            iniciarAPP(respuestaUsuario);
+        } else {
+            console.log(respuestaUsuario);
+            $("#ID_MsjError").show();
         }
-     }
-
-
+    } else {
+        $("#ID_MsjError").show();
+    }
 });
 
 
-function validarFormulario(){
-    //todo esta correcto
-    return true;
+function validarFormulario() {
+    let tokenVal;
+    let aux_Usuario = $("#ID_Usuario").val().trim();
+    let aux_Contrasenia = $("#ID_Contrasenia").val();
+
+    if (aux_Usuario.length > 0 && aux_Usuario != undefined &&
+        aux_Contrasenia.length > 0 && aux_Contrasenia != undefined) {
+        tokenVal = true;
+    } else {
+        tokenVal = false;
+    }
+    return tokenVal;
 }
 
-function validarUsaurio(){
+function iniciarAPP(usuarioLogged){
+    let userAux = usuarioLogged.usuario.usuario
+    window.location = "./modulos/programas/bandeja.html?U="+userAux;
+}
 
-    // va ir base de datos y tratar de hacer match
+function validarUsaurio(usuario, contrasenia) {
 
-    let objetoValidado = {        
-        existe : true,
-        contrasenia : false,
-        usuario: {
-            nombres:"",
-            correo:"",
-            otrosDatos:""
+    let objetoValidado;
+
+    let usuarioRecuperado = lista_Usuario.find(x => x.usuario == usuario);
+
+    try {
+        if (usuarioRecuperado.usuario.length > 0 && usuarioRecuperado != undefined && usuarioRecuperado != null) {
+
+            if (usuarioRecuperado.contrasenia === contrasenia) {
+
+                let userRol = lista_Roles.find(r => r.id == usuarioRecuperado.id_rol);
+
+                objetoValidado = {
+                    existe: true,
+                    contrasenia: true,
+                    usuario: {
+                        nombres: usuarioRecuperado.nombres,
+                        correo: usuarioRecuperado.correo,
+                        dni: usuarioRecuperado.dni,
+                        telefono: usuarioRecuperado.telefono,
+                        usuario: usuarioRecuperado.usuario,
+                        rol: {
+                            nombre: userRol.nombre,
+                            cod: userRol.cod
+                        }
+                    }
+                }
+
+            } else {
+                objetoValidado = {
+                    existe: true,
+                    contrasenia: false,
+                    usuario: null
+                }
+            }
+
+        } else {
+            objetoValidado = {
+                existe: false,
+                contrasenia: false,
+                usuario: null
+            }
+        }
+
+    } catch (error) {
+        console.log(error);
+        objetoValidado = {
+            existe: false,
+            contrasenia: false,
+            usuario: null
         }
     }
+
+
     return objetoValidado;
-    
+
 }
+
+
+var lista_Usuario = [
+    {
+        nombres: "Soy el Admin",
+        correo: "admin@gmail.com",
+        dni: "12345678",
+        telefono: "123456789",
+        usuario: "admin",
+        contrasenia: "admin123456",
+        id_rol: 1
+    },
+    {
+        nombres: "Soy el Docente",
+        correo: "docentet@gmail.com",
+        dni: "12345678",
+        telefono: "123456789",
+        usuario: "docente",
+        contrasenia: "docente123456",
+        id_rol: 2
+    },
+    {
+        nombres: "Soy el Alumno",
+        correo: "alumno@gmail.com",
+        dni: "12345678",
+        telefono: "123456789",
+        usuario: "alumno",
+        contrasenia: "alumno123456",
+        id_rol: 2
+    }
+]
+
+var lista_Roles = [
+    {
+        id: 1,
+        nombre: "Administrador",
+        cod: "ADM"
+    },
+    {
+        id: 2,
+        nombre: "Docente",
+        cod: "DOC"
+    },
+    {
+        id: 3,
+        nombre: "Alumno",
+        cod: "ALU"
+    }
+]
---
+++ recursos/js/programas/programas.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+    getParam();
+});
+
+function getParam() {
+
+    if (true) {
+        iniciarBandeja()
+    }else{
+        window.location = "/index.html"
+    }
+}
+
+function iniciarBandeja() {
+    console.log("Bandeja de programas esta lista para usar")
+}
